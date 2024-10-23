787da53c8e91ff3216da393ffd5caedb1e1991ad
jiin <jiin329@datasolution.kr>
2024-10-23 18:32:43 +0900

    [feat] back-end initial commit

---
+++ back-end/src/test/java/com/speaking_potato/mandarat/mandarat_planner/MandaratPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speaking_potato.mandarat.mandarat_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandalartPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
