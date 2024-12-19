cef46d277cc9f27f020d4d7fc39625e1c7848ede
jiin <jiin329@datasolution.kr>
2024-12-19 15:33:59 +0900

    [feat/fe] delete comment

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -22,8 +22,8 @@ const App: React.FC = () => {
   }, []);
 
   const kakaoLogin = () => {
-    const redirectUri = "redirectUri"; // 카카오 로그인 후 리다이렉트할 URL
-    const clientId = ""
+    const redirectUri = "redirectUri";
+    const clientId = "clienId";
     const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
 
     // 팝업을 열고 카카오 로그인 페이지로 이동
@@ -33,16 +33,11 @@ const App: React.FC = () => {
       "width=500,height=600"
     );
 
-    // 팝업에서 인증 후, 결과를 받아올 때 사용할 메커니즘 (예: 팝업을 닫은 후 부모 창에서 URL을 확인)
     const checkPopupInterval = setInterval(() => {
-      if (popup.closed) {
+      if (popup?.closed) {
         clearInterval(checkPopupInterval);
-        // 팝업이 닫혔을 때, 부모 창에서 인증 결과를 처리할 수 있음
-        // 인증된 코드를 서버로 보내서 토큰을 받아오는 등의 작업을 할 수 있습니다.
       }
     }, 500);
-
-    // https://kauth.kakao.com/oauth/authorize?client_id=895550bfa3b03ce09a479321ec51eb47&redirect_uri=http:%2F%2Flocalhost:8080%2Flogin&response_type=code
   };
 
   return (
