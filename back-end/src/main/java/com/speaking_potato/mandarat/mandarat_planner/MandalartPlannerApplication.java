8b4f5291e709fbc2476a0bf3b0d931758c8c26b2
jiin <jiin329@datasolution.kr>
2025-01-08 15:15:10 +0900

    [feat/be] update for tomcat10 api

--- back-end/src/main/java/com/speaking_potato/mandarat/mandarat_planner/MandalartPlannerApplication.java
+++ back-end/src/main/java/com/speaking_potato/mandarat/mandarat_planner/MandalartPlannerApplication.java
@@ -2,10 +2,17 @@ package com.speaking_potato.mandarat.mandarat_planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
-@SpringBootApplication
-public class MandalartPlannerApplication {
+@SpringBootApplication(scanBasePackages = "com.speaking_potato.mandarat.mandarat_planner")
+public class MandalartPlannerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MandalartPlannerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MandalartPlannerApplication.class, args);
